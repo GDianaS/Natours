cc02334985acd0a77fbcf0cd6fa97d8302a96a32
G. Diana <gab13ssousa@gmail.com>
2025-09-01 20:30:43 -0300

    Update Tour Model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,16 +5,53 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
-        unique: true
+        unique: true,
+        trim: true
     },
-    rating: {
+    duration:{
+        type: Number,
+        required: [true, 'A tour must have a duration']
+    },
+    maxGroupSize:{
+        type: Number,
+        required: [true, 'A tour must have a group size']
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'A tour must have a difficult']
+    },
+    ratingAverage: {
         type: Number,
         default: 4.5,
+    },
+    ratingQuantity:{
+        type: Number,
+        default: 0
     },
     price: {
         type: Number,
         required: [true, 'A tour must have a price']
-    }
+    },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        required: [true, 'A tour must have a summary'],
+        trim: true
+    },
+    description:{
+        type: String,
+        trim: true
+    },
+    imageCover:{
+        type: String,
+        required: [true, 'A tour must have a cover image']
+    },
+    images:[String], // array de strings
+    createdAt:{
+        type: Date,
+        default: Date.now()
+    },
+    startDates:[Date] // array de datas
 });
 // Model a partir do schema
 // Mongoose automaticamente cria a coleção com o nome em minúsculas e plural
