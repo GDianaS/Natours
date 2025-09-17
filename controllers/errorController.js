1a21e6a9ea31ee401529c958dba786a25ed60b01
G. Diana <gab13ssousa@gmail.com>
2025-09-16 21:28:30 -0300

    Errors during Development vs Production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,4 +1,39 @@
 //ERROR HANDLING MIDDLEWARE
+
+const sendErrorDev = (err, res) => {
+    res.status(error.statusCode).json({
+            status: error.status,
+            error: error,
+            message: error.message,
+            stack: error.stack
+        });
+}
+
+
+const sendErrorProd = (err, res) => {
+    // isOperational is defined in appError.js
+    // Operational, trusted error: send message to client.
+    if(err.isOperational){
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+
+    // Programming or other unknown error: don't leak error details to the client.
+    } else {
+        // 1) Log error
+        console.error('ERROR 💥', 'err');
+
+        // 2) Send generic message
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong!'
+        });
+    }
+
+  
+}
+
 module.exports = (error, req, res, next) =>{
 
     //console.log(error.stack);
@@ -7,8 +42,15 @@ module.exports = (error, req, res, next) =>{
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
 
-    res.status(error.statusCode).json({
-        status: error.status,
-        message: error.message
-    });
+    if(process.env.NODE_ENV === 'development'){
+
+        sendErrorDev(error, res);
+
+    } else if(process.env.NODE_ENV === 'production'){
+       
+        sendErrorProd(error, res);
+
+    }
+
+    
 }
